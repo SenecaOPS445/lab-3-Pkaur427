9f2ce7d26b7cb56e9e3cdb24285bc3bc8c13c4bc
Pkaur427 <pk6345422@gmail.com>
2024-06-14 01:12:36 -0400

    Lab3 Completed

---
+++ lab3a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# return_text_value() function
+# Author ID: [seneca_id]
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(number)
+    print(number + 5)
+    print(return_number_value() + 10)
+    print('my number is ' + str(number))
+    print('my number is ' + str(return_number_value()))
+
